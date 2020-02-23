a518553731d63ace731604b74d5a0d3a3e46626c
grisha18 <grishka54321@gmail.com>
2020-02-23 17:57:15 +0300

    comment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './index.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch} from '@fortawesome/free-solid-svg-icons'
 
+import { faAngleDown} from '@fortawesome/free-solid-svg-icons'
+
+
 
 class Button extends React.Component{
 
@@ -13,8 +16,8 @@ class Button extends React.Component{
 
         return(
             
-                    <div style={{  borderWidth: 3, backgroundColor:this.props.backgroundColor,
-                         borderColor: this.props.borderColor, borderStyle: "solid", width: 100, height: 40   }}>
+                    <div style={{  borderWidth: 2, backgroundColor:this.props.backgroundColor,
+                         borderColor: this.props.borderColor, borderStyle: "solid", width: 100, height: 30,  paddingLeft: 15, paddingTop: 12 }}>
                             {this.props.children}
 
                          </div>
@@ -63,19 +66,31 @@ class Header extends React.Component{
                 </div>
             </div>
         </div>
-        <div className='container'>
+        <div className='container'></div>
 
             <div className='privet'>
                 <div className='text'>
-                    
+                    <h1>Web development project</h1>  
                 </div>
+              <div className='test'>
+                  <p>Very suitable to support all web development projects</p>
+              </div>
                 <div className='buttons'>
-                    <Button backgroundColor="transparent" borderColor="red">Press me</Button>
-                    <Button backgroundColor="lightblue" borderColor="transparent"/>
+                    <Button backgroundColor="transparent" borderColor="red">Our Services</Button>
+                    <Button backgroundColor="lightblue" borderColor="transparent">Hire is now</Button>
                 </div>
-            </div>
-        </div>
-    </>
+                <div className='fal'><FontAwesomeIcon icon={faAngleDown}/></div>
+                  <div className='texts'>
+                      <h1>About Us</h1>
+                      <div className='sea'>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor <br></br>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </p>    
+                      </div>
+                          <div className='img'>
+                              <img src='../image/home page.png' alt='hello'/> 
+                         </div>                         
+                     </div>
+       </div>
+     </>
     )
 
   }
